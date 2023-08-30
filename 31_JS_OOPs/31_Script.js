2abf01e57734b51b8f1ecef6fbec26fb61ed180e
thehimanshoo <rajpoothimanshu001@gmail.com>
2023-08-31 00:48:39 +0530

    final commit

---
+++ 31_JS_OOPs/31_Script.js
@@ -0,0 +1,11 @@
+"use strict"
+
+var userName = "JOHN";
+
+export const Hello = () => {
+    return `Hello ! ${userName}`;
+};
+
+export const Welcome = () => {
+    return `Welcome to JavaScript OOP Concept !!`;
+};
