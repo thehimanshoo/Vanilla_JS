fa976bd00fa3ebc9ad3832ed99342417102f48a7
thehimanshoo <rajpoothimanshu001@gmail.com>
2023-09-21 00:40:56 +0530

    final commit

---
+++ 38_JS_Events_Part-3/38_Script.js
@@ -0,0 +1,61 @@
+"use strict"
+
+/*
+
+    Events are classified into various groups, such as:-
+
+        1.  Mouse Events
+        2.  Keyboard Events
+        3.  Button Events
+        4.  Clipboard Events
+        5.  Touch Events
+        6.  Timer Events
+        7.  Elements State Events
+        8.  Miscellaneous Events
+        9.  Form Events
+        10. Window Events
+        11. Document and DOM Events
+        12. Custom Events etc...
+
+
+        ___________ MOUSE EVENTS _______________
+
+        - onclick
+        - ondblclick
+        - onmouseover
+        - onmouseout
+        - onmousedown
+        - onmouseup
+        - onmousemove
+        - onmouseenter
+        - onmouseleave
+
+
+        ___________ KEYBOARD EVENTS _______________
+
+        - onkeydown
+        - onkeyup
+        - onkeypress
+
+
+         ___________ CLIPBOARD EVENTS _______________
+
+        - oncut
+        - oncopy
+        - onpaste
+        - oncontextmenu  => {fot right click}
+        - onselectstart => {fot selection}
+
+*/
+
+const changePosition = (e) => {
+    var flag = document.getElementById("flag");
+    document.getElementById("positionMsg").innerHTML = `
+        Position X = ${e.clientX}
+        Position Y = ${e.clientY}
+    `;
+
+    flag.style.position = "fixed";
+    flag.style.left = `${e.clientX}` + "px";
+    flag.style.top = `${e.clientY}` + "px";
+};
