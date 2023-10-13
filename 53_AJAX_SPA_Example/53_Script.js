5f7397b27d4fe7d383a26fd1eb672ce58e143b65
thehimanshoo <rajpoothimanshu001@gmail.com>
2023-10-13 23:55:28 +0530

    remove unwanted data

--- 53_AJAX_SPA_Example/53_Script.js
+++ 53_AJAX_SPA_Example/53_Script.js
@@ -28,7 +28,6 @@ function LoadView(page) {
         url: "53_AddToCart.html",
         success: (addToCartPage) => {
             $("#AddToCartWrapper").html(addToCartPage);
-            console.log(addToCartPage);
         },
         error: (JqXHR) => {
             console.error(`Error : ${JqXHR.status} ${JqXHR.statusText}`);
